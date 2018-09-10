9be1a631a99ffbb64291327357ccc767d3958286
Erin Dubuc <erin.dubuc9@gmail.com>
2018-09-10 16:45:16 +0000

    lesson6 using require() & a variable to access modules

--- app.js
+++ app.js
@@ -1,18 +1,3 @@
-// normal function statement in Javascript
-// function sayHi(){
-//     console.log('hi');
-// }
-// sayHi();
+var counter = require('./count');
 
-// can pass a function through into another function
-function callFunction(fun){
-    fun();
-}
-
-// function expression in Node
-// stores a function in a variable
-var sayBye = function(){
-    console.log('bye');
-};
-
-callFunction(sayBye);
+console.log(counter(['shaun', 'crystal', 'ryu']));
