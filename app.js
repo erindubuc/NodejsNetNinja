c305f875e024aa3ea5c9ef7346029ce967d0745d
Erin Dubuc <erin.dubuc9@gmail.com>
2018-09-10 17:16:37 +0000

    lesson8 event emitters

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
-var stuff = require('./stuff');
+var events = require('events');
+var util = require('util');
 
-console.log(stuff.counter(['shaun', 'crystal', 'ryu']));
-console.log(stuff.adder(5,6));
-console.log(stuff.adder(stuff.pi,5));
+// create new object constructor
+var Person = function(name){
+    this.name = name;
+};
+
+// want to inherit the event emitter
+// attach events to people
+util.inherits(Person, events.EventEmitter);
+
+var james = new Person('james');
+var mary = new Person('mary');
+var ryu = new Person('ryu');
+var people = [james, mary, ryu];
+
+people.forEach(function(Person){
+    Person.on('speak', function(mssg){
+        console.log(Person.name + ' said: ' + mssg);
+    });
+});
+
+james.emit('speak', 'hey dudes');
+ryu.emit('speak', 'I want a curry');
