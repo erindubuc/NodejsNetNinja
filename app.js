5657a4fa60f74931d530b710bd49a58ea071473b
Erin Dubuc <erin.dubuc9@gmail.com>
2018-09-11 18:11:24 +0000

    lesson12 creating a server with response headers

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
-var fs = require('fs');
- fs.unlink('./stuff/writeMe.txt', function() {
-  fs.rmdir('stuff');   
- });
+var http = require('http');
+
+//create variable to store method
+var server = http.createServer(function(req, res) {
+    console.log('request was made: ' + req.url);
+    // write the response header that contains the status mssg and content-type
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    // end the response with text displayed to user
+    res.end('Hey ninjas');
+});
+
+server.listen(3000, '127.0.0.1'); /*(port number, IP address)*/
+console.log('yo dawgs, now listening to port 3000');
