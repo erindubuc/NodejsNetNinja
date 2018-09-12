68fac514f9ffd783e2661a76d7a748dac96617e1
Erin Dubuc <erin.dubuc9@gmail.com>
2018-09-12 21:08:34 +0000

    lesson16 using a pipe

--- app.js
+++ app.js
@@ -1,24 +1,13 @@
 var http = require('http');
 var fs = require('fs');
 
-var myReadStream = fs.createReadStream(__dirname + '/readme.txt', 'utf8');
-var myWriteStream = fs.createWriteStream(__dirname + '/writeMe.txt');
-
-myReadStream.on('data', function(chunk){
-    console.log('new chunk received:');
-    myWriteStream.write(chunk);
-});
-
-
-
-/*
 var server = http.createServer(function(req, res) {
     console.log('request was made: ' + req.url);
     // write the response header that contains the status mssg and content-type
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    // end the response with text displayed to user
-    res.end('Hey ninjas');
+    var myReadStream = fs.createReadStream(__dirname + '/readme.txt', 'utf8');
+    myReadStream.pipe(res);
 });
 
 server.listen(3000, '127.0.0.1'); /*(port number, IP address)*/
-// console.log('yo dawgs, now listening to port 3000');*/
+console.log('yo dawgs, now listening to port 3000');
