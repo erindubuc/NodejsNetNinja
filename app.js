b79032698c7949d73b3b320a17b4fd987459c4c3
Erin Dubuc <erin.dubuc9@gmail.com>
2018-09-12 20:31:26 +0000

    lesson14 creating areadable stream

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 var http = require('http');
+var fs = require('fs');
 
-//create variable to store method
+var myReadStream = fs.createReadStream(__dirname + '/readme.txt', 'utf8');
+
+myReadStream.on('data', function(chunk){
+    console.log('new chunk received:');
+    console.log(chunk);
+});
+
+
+
+/*
 var server = http.createServer(function(req, res) {
     console.log('request was made: ' + req.url);
     // write the response header that contains the status mssg and content-type
@@ -10,4 +20,4 @@ var server = http.createServer(function(req, res) {
 });
 
 server.listen(3000, '127.0.0.1'); /*(port number, IP address)*/
-console.log('yo dawgs, now listening to port 3000');
+// console.log('yo dawgs, now listening to port 3000');*/
