0e1f5651a9f3f7e6ad8b72d5fdda6429907e1563
Erin Dubuc <erin.dubuc9@gmail.com>
2018-09-13 16:35:01 +0000

    lesson23 responding to requests using express

--- app.js
+++ app.js
@@ -1,23 +1,14 @@
-var http = require('http');
-var fs = require('fs');
+var express = require('express');
+// to get access to the variable express, create app
+var app = express();
 
-var server = http.createServer(function(req, res) {
-    console.log('request was made: ' + req.url);
-    if(req.url === '/home' || req.url === '/'){
-        res.writeHead(200, {'Content-Type' : 'text/html'});
-        fs.createReadStream(__dirname + '/index.html').pipe(res);
-    } else if(req.url === '/contact') {
-       res.writeHead(200, {'Content-Type' : 'text/html'});
-        fs.createReadStream(__dirname + '/contact.html').pipe(res); 
-    } else if (req.url === '/api/ninjas') {
-        var ninjas = [{name: 'ryu', age: 29}, {name: 'yoshi', age: 32}];
-        res.writeHead(200, {'Content-Type' : 'application/json'});
-        res.end(JSON.stringify(ninjas));
-    } else{
-      res.writeHead(200, {'Content-Type' : 'text/html'});
-      fs.createReadStream(__dirname + '/404.html').pipe(res);  
-    }
+// routes that respond to get requests
+// since we're using express, don't need to tell content-type
+app.get('/', function(req, res){
+    res.send('this is the homepage');   
 });
-server.listen(8080, '0.0.0.0');
-// server.listen(3000, '0.0.0.0'); /*(port number, IP address)*/
-console.log('yo dawgs, now listening to port 8080');
+app.get('/contact', function(req, res){
+    res.send('this is the contact page');   
+});
+// need port 8080 for c9
+app.listen(8080);
