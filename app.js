710e38e8fb56d73b148a6d6028de49d42c60b628
Erin Dubuc <erin.dubuc9@gmail.com>
2018-09-13 15:39:01 +0000

    lesson19 basic routing of contacts, 404...

--- app.js
+++ app.js
@@ -3,15 +3,21 @@ var fs = require('fs');
 
 var server = http.createServer(function(req, res) {
     console.log('request was made: ' + req.url);
-    // write the response header that contains the status mssg and content-type
-    res.writeHead(200, {'Content-Type': 'application/json'});
-    var myObj = {
-        name: 'Ryu', 
-        job: 'Ninja',
-        age: 29
-    };
-    res.end(JSON.stringify(myObj));
+    if(req.url === '/home' || req.url === '/'){
+        res.writeHead(200, {'Content-Type' : 'text/html'});
+        fs.createReadStream(__dirname + '/index.html').pipe(res);
+    } else if(req.url === '/contact') {
+       res.writeHead(200, {'Content-Type' : 'text/html'});
+        fs.createReadStream(__dirname + '/contact.html').pipe(res); 
+    } else if (req.url === '/api/ninjas') {
+        var ninjas = [{name: 'ryu', age: 29}, {name: 'yoshi', age: 32}];
+        res.writeHead(200, {'Content-Type' : 'application/json'});
+        res.end(JSON.stringify(ninjas));
+    } else{
+      res.writeHead(200, {'Content-Type' : 'text/html'});
+      fs.createReadStream(__dirname + '/404.html').pipe(res);  
+    }
 });
-
-server.listen(8080, '0.0.0.0'); /*(port number, IP address)*/
+server.listen(8080, '0.0.0.0');
+// server.listen(3000, '0.0.0.0'); /*(port number, IP address)*/
 console.log('yo dawgs, now listening to port 8080');
