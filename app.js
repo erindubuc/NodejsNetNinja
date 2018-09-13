9067ad6d4e7fb40688a8299dfe485f5079573cd6
Erin Dubuc <erin.dubuc9@gmail.com>
2018-09-13 16:45:01 +0000

    lesson24 using Express route params

--- app.js
+++ app.js
@@ -10,5 +10,9 @@ app.get('/', function(req, res){
 app.get('/contact', function(req, res){
     res.send('this is the contact page');   
 });
+
+app.get('/profile/:name', function(req, res){
+    res.send('You requested to see a profile with the name of ' + req.params.name);
+});
 // need port 8080 for c9
 app.listen(8080);
