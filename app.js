d0d5d01a37d09e2e3af5ee5166ff89d4d660337d
Erin Dubuc <erin.dubuc9@gmail.com>
2018-09-15 15:17:39 +0000

    lesson26 added more data into view using Javascript

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.get('/contact', function(req, res){
 
 app.get('/profile/:name', function(req, res){
     // when a user queries a profile, we can insert more data into the view
-    var data = {age: 29, job: 'ninja'};
+    var data = {age: 29, job: 'ninja', hobbies: ['eating', 'fighting', 'fishing']};
     res.render('profile', {person: req.params.name, data: data});
 });
 // need port 8080 for c9
