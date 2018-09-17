73ef7e79d9d3bbb4c4d1373a2b52feb6a3cd250b
Erin Dubuc <erin.dubuc9@gmail.com>
2018-09-17 00:10:54 +0000

    trying to fix err

--- public/app.js
+++ public/app.js
@@ -5,12 +5,12 @@ var app = express();
 // set up template engine
 app.set('view engine', 'ejs');
 
-// serve up static files using middleware
+// serve up static files 
 app.use(express.static('./public'));
 
 // fire controllers
 todoController(app);
 
 // listen to port
-app.listen(8080);
+app.listen(process.env.port, 8080);
 console.log('you are listening to port 8080');
